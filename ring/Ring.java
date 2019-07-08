41df4938b70a132548fbe4ab91d4af05e90dd96f
Elena Rubleva <ellena.hella@yandex.ru>
2019-07-08 23:16:17 +0500

    Мелкие правки

--- ring/Ring.java
+++ ring/Ring.java
@@ -5,7 +5,7 @@ import java.io.*;
 
 public class Ring {
     private static ArrayList<Double> data = new ArrayList<>();
-    private static void getData() throws IOException{
+    private static void getData() throws IOException{ // Получение данных
         FileReader file = new FileReader("input.txt");
         Scanner sc = new Scanner(file);
         String str = sc.nextLine();
